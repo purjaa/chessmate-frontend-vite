839455f5d7cc182921ad3e0b0405b9821938a631
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-09 17:38:29 +0200

    install and configure tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./main.tsx",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
