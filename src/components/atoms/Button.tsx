f75cf9a34462b004baa104c590e7ec3977916468
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-11 18:52:52 +0200

    implement a theme for button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { ButtonTheme } from '../theme';
+import clsx from 'clsx';
+import { ButtonThemeType, ButtonThemeSize } from '../theme';
 
 export enum ButtonType {
   Primary = 'primary',
   Secondary = 'secondary'
 }
 
+export enum ButtonSize {
+  Normal = 'normal',
+  Small = 'small',
+  Large = 'large'
+}
+
 export type ButtonProps = {
-  type?: ButtonType
-  disabled?: boolean
+  type?: ButtonType;
+  size?: ButtonSize
+  disabled?: boolean;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 function Button(props: ButtonProps) {
-  const { children, type, ...rest } = props;
+  const { children, type, size, ...rest } = props;
 
   return (
     <button
-      className={ButtonTheme[type ?? ButtonType.Primary]}
+      className={clsx(ButtonThemeType['common'], ButtonThemeType[type ?? ButtonType.Primary], ButtonThemeSize[size ?? ButtonSize.Normal])}
       {...rest}>
         {children}
     </button>
--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,4 +1,21 @@
-export const ButtonTypeTheme = {
-  primary: "bg-blue-500 hover:bg-blue-700 text-white font-bold rounded",
-  secondary: "bg-red-500 hover:bg-blue-700 text-white font-bold rounded"
+export const ButtonThemeType = {
+  common: [
+    'tw-rounded-md', 'tw-text-center', 'tw-font-medium', 'tw-transition', 'tw-duration-2'
+  ],
+  primary: [
+    'tw-bg-ocean-500', 'tw-text-white',
+    'hover:tw-bg-ocean-600', 
+    'active:tw-bg-ocean-700'
+  ],
+  secondary: [
+    'tw-bg-white', 'tw-border-2', 'tw-border-gray-500', 
+    'hover:tw-bg-gray-200', 
+    'active:tw-bg-gray-300'
+  ],
 };
+
+export const ButtonThemeSize = {
+  normal: ['tw-min-w-[145px]', 'tw-px-4', 'tw-py-1.5'],
+  small: ['tw-min-w-[75px]', 'tw-px-2', 'tw-py-0.5'],
+  large: ['tw-min-w-[175px]', 'tw-px-6', 'tw-py-2.5']
+}
