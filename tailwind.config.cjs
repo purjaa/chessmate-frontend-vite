601709d2866868b60ac998a64a95e84a568685c3
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-11 18:20:22 +0200

    configure prefix for tailwindcss classes, add theme definitions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./main.tsx",
-    "./src/**/*.{ts,tsx}",
+    './main.tsx',
+    './src/**/*.{ts,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  prefix: 'tw-',
+  theme: {
+    extend: {
+      colors: {
+        ocean: {
+          200: '#ecf7f9',
+          300: '#ddeef2',
+          400: '#8ec7d4',
+          500: '#008eaa',
+          600: '#036d82',
+          700: '#004755'
+        }
+      }
+    },
+  },
 }
