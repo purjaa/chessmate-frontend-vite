00aaa4bc9d0971ab397c10051c3830b8086049cb
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-12 16:05:43 +0200

    add fullWidth parameter to button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -5,30 +5,36 @@ import { ButtonThemeType, ButtonThemeSize } from '../theme';
 export enum ButtonType {
   Primary = 'primary',
   Secondary = 'secondary'
-}
+};
 
 export enum ButtonSize {
   Regular = 'regular',
   Small = 'small',
   Large = 'large'
-}
+};
 
 export type ButtonProps = {
   type?: ButtonType;
-  size?: ButtonSize
+  size?: ButtonSize;
+  fullWidth?: boolean;
   disabled?: boolean;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 function Button(props: ButtonProps) {
-  const { children, type, size, ...rest } = props;
+  const { children, type, size, fullWidth, ...rest } = props;
 
   return (
     <button
-      className={clsx(ButtonThemeType['common'], ButtonThemeType[type ?? ButtonType.Primary], ButtonThemeSize[size ?? ButtonSize.Regular])}
+      className={clsx(
+        ButtonThemeType['common'],
+        ButtonThemeType[type ?? ButtonType.Primary],
+        ButtonThemeSize[size ?? ButtonSize.Regular],
+        fullWidth && 'tw-w-full'
+      )}
       {...rest}>
         {children}
     </button>
-  )
+  );
 }
 
 export default Button;
