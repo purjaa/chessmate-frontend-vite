8d4c4c5748a12e1b37c87e76a5f6d3481ca7e598
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-13 21:53:46 +0200

    make small adjustments to LoginForm

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -5,11 +5,16 @@ import TextInput from '../atoms/TextInput';
 function LoginForm() {
   return (
     <div className='tw-flex tw-flex-col tw-bg-white tw-gap-y-4 tw-w-1/4 tw-p-4 tw-rounded-mw tw-shadow-lg'>
-      <h1>Log In</h1>
+      <h3 className='tw-text-center'>Log Into Chessmate</h3>
       <TextInput label='Username' />
       <TextInput label='Password' />
       <div className='tw-mt-2'>
-        <Button size={ButtonSize.Regular} fullWidth={true}>Log in</Button>
+        <Button
+          size={ButtonSize.Regular}
+          fullWidth={true}
+        >
+          Log In
+        </Button>
       </div>
     </div>
   );
