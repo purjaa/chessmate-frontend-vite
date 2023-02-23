05537b1e6d26d4daefcde339906fa29a9b6f32b3
Jaakko Purhonen <jaakko.purhonen@gmail.com>
2023-02-23 18:08:10 +0200

    restyling Log In and Registration pages

--- src/components/CommonStyle.ts
+++ src/components/CommonStyle.ts
@@ -1,11 +1,11 @@
 export const InputFormStyle = [
   'tw-flex',
   'tw-flex-col',
-  'tw-my-auto',
   'tw-bg-white',
+  'tw-mt-10',
   'tw-gap-y-4',
   'tw-w-1/4',
   'tw-p-4',
-  'tw-rounded-mw',
+  'tw-rounded-md',
   'tw-shadow-lg'
 ];
--- src/pages/CommonStyle.ts
+++ src/pages/CommonStyle.ts
@@ -1,5 +1,4 @@
 export const PublicPageStyle = [
   'tw-flex',
-  'tw-h-full',
   'tw-justify-center'
 ];
